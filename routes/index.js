9d1c5a20ba1afb53d2ab828fc99948555c800f32
apeeyush <peeyushagarwal1994@gmail.com>
2015-07-15 21:37:07 +0530

    Init code structure for coffee
    Create 3 pages - Home, About and Contact
    Add a basic form in Home page - submit button prints data on console
    Also add example feeds on home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'COmplete your Favourite FEEd' });
+});
+
+/* POST home page */
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.render('index', { title: 'COmplete your Favourite FEEd' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET home page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
